93d365f8c32926f9d038f2f5ead86f045305765b
Bastien Mangin <bmangin@z4r2p7.42lyon.fr>
2022-05-23 14:42:24 +0200

    fucking include

--- includes/tools/iterator.hpp
+++ includes/tools/iterator.hpp
@@ -1,7 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   iterator.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bmangin <bmangin@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/23 14:35:31 by bmangin           #+#    #+#             */
+/*   Updated: 2022/05/23 14:36:17 by bmangin          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #pragma once
+
 #include "tools/iterator_traits.hpp"
 
-template<class Category, class T, class Distance, class Pointer, class Reference>
+template < class Category, class T, class Distance, class Pointer, class Reference >
 struct ft::base_iterator
 {
 	typedef T			value_type;
